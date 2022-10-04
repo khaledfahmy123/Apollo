3603a5d2f2e5905df103f57d376fe31760805e60
Khaled Fahmy Mohamed Abdelaziz <khaledfahmy345@gmail.com>
2022-10-04 21:13:00 +0200

    final v1

--- src/compos/loader/loader.js
+++ src/compos/loader/loader.js
@@ -1,18 +1,20 @@
-import styles from "./loader.module.css"
-
-
-
-
+import styles from "./loader.module.css";
 
 const Loader = () => {
-    return <>
-        <section className={styles.loader}>
-            <div>
-            lol
-            </div>
-            <img src={require("./../../imgs/loaders/a (9).gif")}/>
-        </section>
+  return (
+    <>
+      <section className={styles.loader}>
+        {/* <img src={require("./../../imgs/loaders/a (9).gif")}/> */}
+        <div class={styles["lds-ring"]}>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className={styles.txt}>Loading...</div>
+      </section>
     </>
-}
+  );
+};
 
 export default Loader;
--- src/compos/tabs/timline/compos/object/object.js
+++ src/compos/tabs/timline/compos/object/object.js
@@ -1,20 +1,33 @@
-import React, { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
+import React, { Suspense, useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { useGLTF } from "@react-three/drei";
 import { OrbitControls, Stars } from "@react-three/drei";
 
 const Obj = () => {
+  const obj = useRef();
+  useFrame(() => {
+    obj.current.rotation.y += 0.001;
+  });
   const gltf = useGLTF(
     require("./../../../../../assets/models/ISS_stationary.glb")
   );
-  return <primitive object={gltf.scene} scale={0.08} />;
+  return <primitive ref={obj} object={gltf.scene} scale={0.08} />;
 };
 
 export const Object = (props) => {
   return (
     <>
       <Suspense fallback={null}>
-        <Canvas>
+        <Canvas camera={{ position: [1, 1, 8] }}>
+          <color attach="background" args={["black"]} />
+          <Stars
+        radius={300}
+        depth={60}
+        count={20000}
+        factor={7}
+        saturation={0}
+        fade={true}
+      />
           <pointLight position={[10, 10, 10]} />
           <Obj />
           <OrbitControls />
